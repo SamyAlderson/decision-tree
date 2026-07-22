d6bb3fd15a82ed3024dcebd1846252db8ffce45a
SamyAlderson <samysam35000@gmail.com>
2026-07-22 13:59:29 +0200

    the original approach was fundamentally broken, started over

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='decision-tree',
+    version='1.0',
+    description='A basic decision tree implementation in Python for data science tasks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/samy-alderson/decision-tree',
+    author='Samy Alderson',
+    author_email='samy.alderson@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['numpy', 'pandas'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
